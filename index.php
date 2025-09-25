e059be33d58c01b183768c0f59db535fbb4eea77
Mar2684 <154634810+Mar2684@users.noreply.github.com>
2025-09-25 22:16:32 +0200

    New version
    ajout d'une nouvelle page traçabilité

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     <header>
         <a href="index.php?page=cuve" id="accueil">Accueil</a>
         <a href="index.php?page=historique" id="historique">Historique</a>
-        <a href="contact">Contact</a>
+        <a href="index.php?page=traçabilité">Traçabalité</a>
     </header>
     <main>
         <?php
---
+++ traçabilité.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="traçabilité.css">
+  </head>
+  <body>
+    <div id="container1">
+      <input type="text" name="numéroLot" id="num_lot"s>
+      <label for="numéroLot">Numéro de lot</label>
+    </div>
+  </body>
+</html>
